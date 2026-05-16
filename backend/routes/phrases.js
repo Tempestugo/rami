565e5dfe849914086cf3b9bedb02b85a363feb2f
Tempestugo <coffeture.blog@gmail.com>
2026-05-16 16:49:23 -0300

    limpeza

--- backend/routes/phrases.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-const { buildPhrase } = require(path.join(__dirname, '..', 'controllers', 'phraseController'));
-
-// POST /api/phrases/build
-router.post('/build', buildPhrase);
-
-module.exports = router;
