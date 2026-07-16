adc2571eb337dee29d7665ac00ee62e0976a5028
Tempestugo <coffeture.blog@gmail.com>
2026-07-15 22:59:12 -0300

    Revert proxy bypass in vite.config.js that was causing 503 errors

--- vite.config.js
+++ vite.config.js
@@ -12,14 +12,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:3000',
-        bypass: (req) => {
-          if (req.url.startsWith('/api/_data')) {
-            return req.url;
-          }
-        }
-      }
+      '/api': 'http://127.0.0.1:3000'
     }
   },
   optimizeDeps: { 
